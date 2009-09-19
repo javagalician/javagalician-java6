638cac3869aed5ff38aaa9b48475765ab6179347
Daniel Fernández <daniel.fernandez@11thlabs.org>
2009-09-19 14:02:43 +0000

    Several changes made
    git-svn-id: https://javagalician.svn.sourceforge.net/svnroot/javagalician/javagalician-java6@29 7ec82e3c-ccda-4a0b-9fe4-c15a8fd7ab9c

--- src/main/java/org/javagalician/java6/util/spi/GalicianLocaleNameProvider.java
+++ src/main/java/org/javagalician/java6/util/spi/GalicianLocaleNameProvider.java
@@ -1,7 +1,7 @@
 /*
  * =============================================================================
  * 
- *   Copyright (c) 2008, The JAVAGALICIAN team (http://www.javagalician.org)
+ *   Copyright (c) 2009, The JAVAGALICIAN team (http://www.javagalician.org)
  * 
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
